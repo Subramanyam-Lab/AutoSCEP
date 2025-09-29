6214924da29ca0e1944884f9b58113858df76184
Taehyeon <kwon981224kwon@gmail.com>
2025-09-29 01:05:42 -0400

    0928updates

--- codes/Experiments/main_bm.sh
+++ codes/Experiments/main_bm.sh
@@ -2,19 +2,18 @@
 #SBATCH --nodes=1                          
 #SBATCH --ntasks=5                         
 #SBATCH --cpus-per-task=8                  
-#SBATCH --mem=500G
+#SBATCH --mem=64G
 #SBATCH --time=3:00:00
 #SBATCH --account=azs7266_sc
 #SBATCH --partition=sla-prio
 #SBATCH --job-name=bd_empire
-#SBATCH --output=logs/full_ph_persistent_empire_%A_%a.out  
-#SBATCH --error=logs/full_ph_persistent_empire_%A_%a.err   
+#SBATCH --output=logs/ph_%a.out  
+#SBATCH --error=logs/ph_%a.err   
 #SBATCH --array=0-9
 
 METHOD="PH"
 SEEDS=(11 12 13 14 15 16 17 18 19 20)
-# TIMES=(60 300 600 1800 3600)
-TIMES=(3600)
+TIMES=(60 300 600 1800 3600)
 
 NUM_SEEDS=${#SEEDS[@]}
 
@@ -26,7 +25,7 @@ TIME=${TIMES[$TIME_INDEX]}
 
 
 source ~/.bashrc
-conda activate neurmhsp
+conda activate myenv
 module purge
 module load gurobi/10.0.3
 module load gcc/13.2.0
@@ -36,7 +35,6 @@ export MPICH_ASYNC_PROGRESS=1
 
 echo "Job Array ID: $SLURM_ARRAY_JOB_ID, Task ID: $SLURM_ARRAY_TASK_ID"
 echo "Running on nodes: $SLURM_NODELIST"
-# TIMELIMIT 변수명이 TIME으로 변경되었으므로 srun 명령어에서도 수정합니다.
 echo "SEED=$SEED, NUMSCE=$SLURM_NTASKS, METHOD=$METHOD, TIMELIMIT=$TIME"
 
 srun python empire_bm.py --seed $SEED --num-sce $SLURM_NTASKS --method $METHOD --time $TIME
--- codes/Experiments/main_ef.sh
+++ codes/Experiments/main_ef.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=500G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
 #SBATCH --time=24:00:00
 #SBATCH --job-name=neurumhsp
-#SBATCH --output=logs/full_ef5_empire_%a.out
-#SBATCH --error=logs/full_ef5_empire_%a.err
+#SBATCH --output=logs/ef_%a.out
+#SBATCH --error=logs/ef_%a.err
 #SBATCH --account=azs7266_sc
 #SBATCH --partition=sla-prio
-#SBATCH --array=6
+#SBATCH --array=1-10
 
 source ~/.bashrc
 
-conda activate neurmhsp
+conda activate myenv
 module purge
 module load gurobi/10.0.3
 module load cuda/11.5.0
@@ -24,7 +24,7 @@ export MPICH_ASYNC_PROGRESS=1
 echo "Running on nodes: $SLURM_NODELIST (Array Task: $SLURM_ARRAY_TASK_ID)"
 
 seeds=(11 12 13 14 15 16 17 18 19 20)
-numsces=(5)
+numsces=(5 10 20)
 
 idx=$(( SLURM_ARRAY_TASK_ID - 1 ))
 
@@ -34,4 +34,3 @@ NUMSCE=${numsces[$(( idx / ${#seeds[@]} ))]}
 echo "SEED=$SEED, NUMSCE=$NUMSCE"
 
 python empire_ef.py --seed $SEED --num-sce $NUMSCE
-# python empire_ef_optimal.py
